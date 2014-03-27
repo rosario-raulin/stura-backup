40279f5885b777fb1fa4319aac0153818e434ab9
Rosario Raulin <post@rosario-raulin.de>
2014-03-27 13:35:55 +0100

    add logging

--- backup.sh
+++ backup.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-BACKUP_DIR = "backup"
-ROOT_MAIL = "rosario.raulin@stura-md.de"
+BACKUP_DIR="backup"
+ROOT_MAIL="rosario.raulin@stura-md.de"
 
-BACKUP_DIR_ERROR = "The backup directory of stura-md.de couldn't be found!"
+log_error() {
+  subject = $1
+  message = $2
+  echo ${message} | mail -s ${subject} ${ROOT_MAIL}
+}
 
 cd /
 
 mkdir -p ${BACKUP_DIR}
 
-if [true]; then
-  mail -s "Error creating backup file" ${ROOT_MAIL} << ${BACKUP_DIR}
-  . exit 1
+if [ -d "$BACKUP_DIR" ]; then
+  log_error "[Server Backup] Error creating backup dir" ""
 fi
